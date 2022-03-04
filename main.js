c6b3c315ff0ff2ff741cdc8f967ec96e692e1680
Olexander Tougaj <sashamolodec@gmail.com>
2022-03-04 13:19:58 +0200

    Создано поведение страницы.

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 $(function () {
-	const mm = moment('2022-03-02 07:16', 'YYYY-MM-DD HH:mm');
-	console.log(mm.toISOString());
+	$("#editDate").on("change", printUTCDate);
+	$("#editTime").on("change", printUTCDate);
+	$("#formTimeStamp").on("submit", onTimeStampSubmit);
+	$("#btnReset").on("click", resetToCurrentDateTime);
+	resetToCurrentDateTime();
 });
+
+const printUTCDate = () => {
+	const date = $("#editDate").val();
+	const time = $("#editTime").val();
+	const mm = moment(`${date} ${time}`);
+	$("#utcDate").val(mm.toISOString());
+};
+
+const onTimeStampSubmit = (event) => {
+	event.preventDefault();
+	printUTCDate();
+};
+
+const resetToCurrentDateTime = () => {
+	const mmCurrent = moment();
+	$("#editDate").val(mmCurrent.format("YYYY-MM-DD"));
+	$("#editTime").val(mmCurrent.format("HH:mm"));
+	printUTCDate();
+};
