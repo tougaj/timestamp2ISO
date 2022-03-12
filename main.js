e5a84af2b7eeef9e7df19d39aac16810b1b9a722
tougaj <sashamolodec@gmail.com>
2022-03-12 17:05:44 +0200

    Додано можливість конвертувати градуси в десятичні значення.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 $(function () {
-	$("#editDate").on("change", printUTCDate);
-	$("#editTime").on("change", printUTCDate);
-	$("#formTimeStamp").on("submit", onTimeStampSubmit);
-	$("#btnReset").on("click", resetToCurrentDateTime);
+	$('#editDate').on('change', printUTCDate);
+	$('#editTime').on('change', printUTCDate);
+	$('#formTimeStamp').on('submit', onTimeStampSubmit);
+	$('#btnReset').on('click', resetToCurrentDateTime);
+	$('#formLat input[name]').add('#formLon input[name]').on('change', onDegreeChange);
 	resetToCurrentDateTime();
+	onDegreeChange();
 });
 
 const printUTCDate = () => {
-	const date = $("#editDate").val();
-	const time = $("#editTime").val();
+	const date = $('#editDate').val();
+	const time = $('#editTime').val();
 	const mm = moment(`${date} ${time}`);
-	$("#utcDate").val(mm.toISOString());
+	$('#utcDate').val(mm.toISOString());
 };
 
 const onTimeStampSubmit = (event) => {
@@ -20,8 +22,22 @@ const onTimeStampSubmit = (event) => {
 
 const resetToCurrentDateTime = () => {
 	const mmCurrent = moment();
-	$("#editDate").val(mmCurrent.format("YYYY-MM-DD"));
-	$("#editTime").val(mmCurrent.format("HH:mm"));
+	$('#editDate').val(mmCurrent.format('YYYY-MM-DD'));
+	$('#editTime').val(mmCurrent.format('HH:mm'));
 	printUTCDate();
-	$("#utcDate").focus().select();
+	$('#utcDate').focus().select();
+};
+
+const getDegree = (form) =>
+	(
+		parseInt(form['degrees'].value) +
+		parseInt(form['minutes'].value) / 60 +
+		parseInt(form['seconds'].value) / 3600
+	).toLocaleString('en-US', { maximumFractionDigits: 8 });
+
+const onDegreeChange = () => {
+	document.getElementById('editDegreeNumeric').value = [
+		getDegree(document.getElementById('formLat')),
+		getDegree(document.getElementById('formLon')),
+	].join(', ');
 };
